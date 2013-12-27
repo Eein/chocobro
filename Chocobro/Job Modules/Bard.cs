e1c930c75471a77a8164c5c556d7fb076206e9ca
chordscollide <chordscollide@gmail.com>
2013-12-27 14:53:19 -0600

    Update Bard.cs

--- Chocobro/Job Modules/Bard.cs
+++ Chocobro/Job Modules/Bard.cs
@@ -27,7 +27,9 @@ namespace Chocobro {
         // -------------------
 
         // Heavy Shot
-        Ability heavyshot = new Ability() { name = "Heavy Shot", potency = 150, dotPotency = 0, recastTime = 2.5, abilityCost = 70, animationDelay = 0.5, abilityType = "Weaponskill", castTime = 0.0, duration = 0.0 };
-        
+        Ability heavyshot = new Ability() { name = "Heavy Shot", potency = 150, dotPotency = 0, recastTime = 2.5, abilityCost = 60, animationDelay = 0.3, abilityType = "Weaponskill", castTime = 0.0, duration = 0.0 };
+        Ability windbite = new Ability() { name = "Windbite", potency = 60, dotPotency = 45, recastTime = 2.5, abilityCost = 80, animationDelay = 0.3, abilityType = "Weaponskill", castTime = 0.0, duration = 18 };
+        Ability venbite = new Ability() { name = "Venemous Bite", potency = 100, dotPotency = 35, recastTime = 2.5, abilityCost = 80, animationDelay = 0.3, abilityType = "Weaponskill", castTime = 0.0, duration = 18 };
+        Ability straightshot = new Ability() { name = "Straight Shot", potency = 140, dotPotency = 0, recastTime = 2.5, abilityCost = 70, animationDelay = 0.3, abilityTyp = "Weaponskill", castTime = 0.3, duration = 20 };
     }
 }
