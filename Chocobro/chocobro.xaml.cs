dd318d7f29d79b9db0eab037043eb3ea806a4c4d
Eein <eeinblack@live.com>
2014-01-06 15:17:09 -0600

    Update chocobro.xaml.cs

--- Chocobro/chocobro.xaml.cs
+++ Chocobro/chocobro.xaml.cs
@@ -103,7 +103,7 @@ namespace Chocobro {
 
       clearLog();
       Job p = new Job();
-      Bard b = new Bard();
+      Bard b = new Bard(p);
       p.name = "Job Not Defined"; // Debug text.
 
       //Define Player Object
