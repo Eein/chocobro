a7b5e239e0bd8e936e4d5f716fc25da3df4c84ee
Eein <williamvolin@live.com>
2014-02-23 16:48:48 -0600

    Damage sources chart added

--- Chocobro/Sim Base/Report.cs
+++ Chocobro/Sim Base/Report.cs
@@ -9,7 +9,10 @@ namespace Chocobro {
   class Report {
     string report = "";
     string dpetcolor = "006699";
+    double fightlength = MainWindow.fightlength;
     public void parse(Job j) {
+      var dps = j.totaldamage / MainWindow.fightlength;
+
       report += "<!doctype html><html class='no-js' lang='en'><head><title>Chocobro Report</title><style>";
       //style
       report += "meta.foundation-version {font-family:'/5.1.0/';}meta.foundation-mq-small {font-family:'/only screen and (max-width: 40em)/';width:0em;}meta.foundation-mq-medium {font-family:'/only screen and (min-width:40.063em)/';width:40.063em;}meta.foundation-mq-large {font-family:'/only screen and (min-width:64.063em)/';width:64.063em;}meta.foundation-mq-xlarge {font-family:'/only screen and (min-width:90.063em)/';width:90.063em;}meta.foundation-mq-xxlarge {font-family:'/only screen and (min-width:120.063em)/';width:120.063em;}meta.foundation-data-attribute-namespace {font-family:false;}html ,body {height:100%;}* ,*:before ,*:after {-moz-box-sizing:border-box;-webkit-box-sizing:border-box;box-sizing:border-box;}html ,body {font-size:100%;}body {background:#ffffff;color:#222222;padding:0;margin:0;font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-weight:normal;font-style:normal;line-height:1;position:relative;cursor:default;}a:hover {cursor:pointer;}img ,object ,embed {max-width:100%;height:auto;}object ,embed {height:100%;}img {-ms-interpolation-mode:bicubic;}#map_canvas img ,#map_canvas embed ,#map_canvas object ,.map_canvas img ,.map_canvas embed ,.map_canvas object {max-width:none !important;}.left {float:left !important;}.right {float:right !important;}.clearfix {*zoom:1;}.clearfix:before ,.clearfix:after {content:' ';display:table;}.clearfix:after {clear:both;}.hide {display:none;}.antialiased {-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;}img {display:inline-block;vertical-align:middle;}textarea {height:auto;min-height:50px;}select {width:100%;}body {width:100%;margin-left:auto;margin-right:auto;margin-top:0;margin-bottom:0;max-width:62.5rem;*zoom:1;background-color:#333333;color:#ffffff;font-size:14px;}body:before ,body:after {content:' ';display:table;}body:after {clear:both;}a {text-decoration:none;}a:visited ,a:link ,a:active {color:#006699;}a:hover {color:#ffffff;}.tile ,.info ,.stats ,.weights ,.dpet ,.dpstimeline ,.damagesources ,.blank ,.abilities ,.buffs ,.debuffs {background-color:#222222;padding:10px;min-height:540px;border:2px solid #000000;}.wrapper {background-color:#333333;width:100%;margin-left:auto;margin-right:auto;margin-top:0;margin-bottom:0;max-width:62.5rem;*zoom:1;border-left:2px solid #000000;border-right:2px solid #000000;height:auto;}.wrapper:before ,.wrapper:after {content:' ';display:table;}.wrapper:after {clear:both;}.header ,.footer {width:100%;margin-left:auto;margin-right:auto;margin-top:0;margin-bottom:0;max-width:62.5rem;*zoom:1;background-color:#000000;font-size:18px;padding:10px;}.header:before ,.header:after {content:' ';display:table;}.header:after {clear:both;}.info {padding-left:0.9375rem;padding-right:0.9375rem;width:50%;float:left;}.stats {padding-left:0.9375rem;padding-right:0.9375rem;width:50%;float:left;}.weights {padding-left:0.9375rem;padding-right:0.9375rem;width:100%;float:left;min-height:300px;}.weights table {background-color:#000000;}.weights table thead {background-color:#000000;}.weights table tbody tr:nth-child(even) {background:#444444;}.weights table tbody tr:nth-child(odd) {background:#111111;}.dpet {padding-left:0.9375rem;padding-right:0.9375rem;width:50%;float:left;}.dpstimeline {padding-left:0.9375rem;padding-right:0.9375rem;width:50%;float:left;}.damagesources {padding-left:0.9375rem;padding-right:0.9375rem;width:50%;float:left;}.blank {padding-left:0.9375rem;padding-right:0.9375rem;width:50%;float:left;}.abilities {padding-left:0.9375rem;padding-right:0.9375rem;width:100%;float:left;min-height:300px;}.buffs {padding-left:0.9375rem;padding-right:0.9375rem;width:50%;float:left;}.debuffs {padding-left:0.9375rem;padding-right:0.9375rem;width:50%;float:left;}.footer {font-size:12px;}.footer .social {float:right;}";
@@ -69,7 +72,32 @@ namespace Chocobro {
           
 
         }
-      report += "\" /></div></div><div class='dpstimeline'><h3>Timeline</h3></div></div><div class='wrapper'><div class='damagesources'><h3>Damage Sources</h3><img src='//chart.googleapis.com/chart?chxs=0,FFFFFF,11.5&&chco=C74C4C&&chxt=x&&chs=500x225&chts=FFFFFF,18&chf=bg,s,00000000&cht=p&chd=t:' + dpspercent(heavyshot.totaldamage, damagepersecond, fightlength) + ',' + dpspercent(straightshot.totaldamage, damagepersecond, fightlength) + ',' + dpspercent(bloodletter.totaldamage, damagepersecond, fightlength) + ',' + dpspercent(venomousbite.totaldamage, damagepersecond, fightlength) + ',' + dpspercent(venomousbite.dottotaldamage, damagepersecond, fightlength) + ',' + dpspercent(windbite.totaldamage, damagepersecond, fightlength) + ',' + dpspercent(windbite.dottotaldamage, damagepersecond, fightlength) + ',' + dpspercent(autoattack.totaldamage, damagepersecond, fightlength) + ',' + dpspercent(miserysend.totaldamage, damagepersecond, fightlength) +',' + dpspercent(flamingarrow.dottotaldamage, damagepersecond, fightlength) + '&chl=Heavy Shot|Straight Shot|Bloodletter|Venomous Bite|Venomous Bite Dot|Windbite|Windbite Dot|Auto Attack|Miserys End|Flaming Arrow&chtt=Damage+Sources' width='450 height='225'/></div><div class='blank'><h3>Blank</h3></div></div><div class='wrapper'><div class='abilities'><h3>Ability Breakdown</h3></div></div><div class='wrapper'><div class='buffs'><h3>Buffs</h3></div><div class='debuffs'><h3>Debuffs</h3></div></div><div class='footer'>Chocobro © 2013-2014. FINAL FANTASY XIV © 2010-2014 SQUARE ENIX CO., LTD. All Rights Reserved. <div class='social'><a href='http://github.com/eein/chocobro' target='_blank'>Github</a> - <a href='http://www.twitter.com/chocobrodotcom' target='_blank'>Twitter</a> - <a href='http://www.chocobro.com' target='_blank'>Homepage</a></div></div><script src='js/app.js'></script></body></html>";
+      report += "\" /></div></div><div class='dpstimeline'><h3>Timeline</h3></div></div><div class='wrapper'><div class='damagesources'><h3>Damage Sources</h3>";
+      report += "<img src='http://chart.googleapis.com/chart?chxs=0,FFFFFF,11.5&&chco=C74C4C&&chxt=x&&chs=500x225&chts=FFFFFF,18&chf=bg,s,00000000&cht=p&chd=t:";
+      
+       foreach (Ability ability in j.areport){
+          if (ability.damage > 0) { //checks to make sure abilities do damage....
+            if(ability.dotPotency > 0){
+              report += dpspercent(ability.dotdamage, dps, fightlength) + ",";
+            }
+            report += dpspercent(ability.damage, dps, fightlength) + ",";
+          }
+        }
+       // eating the last comma off the end of the string ..
+       report = report.Substring(0, report.Length - 1);
+      report += "&chl=";
+       foreach (Ability ability in j.areport) {
+         if (ability.damage > 0) { //checks to make sure abilities do damage....
+           if (ability.dotPotency > 0) {
+             report += ability.name + " Dot|";
+           }
+           report += ability.name + "|";
+         }
+       }
+      // eating the last pipe off the end of the string ..
+      report = report.Substring(0, report.Length - 1);
+
+      report += "&chtt=Damage+Sources' width='450 height='225'/></div><div class='blank'><h3>MP/TP Regen (NA)</h3></div></div><div class='wrapper'><div class='abilities'><h3>Ability Breakdown</h3></div></div><div class='wrapper'><div class='buffs'><h3>Buffs</h3></div><div class='debuffs'><h3>Debuffs</h3></div></div><div class='footer'>Chocobro © 2013-2014. FINAL FANTASY XIV © 2010-2014 SQUARE ENIX CO., LTD. All Rights Reserved. <div class='social'><a href='http://github.com/eein/chocobro' target='_blank'>Github</a> - <a href='http://www.twitter.com/chocobrodotcom' target='_blank'>Twitter</a> - <a href='http://www.chocobro.com' target='_blank'>Homepage</a></div></div><script src='js/app.js'></script></body></html>";
       
       write();
     }
@@ -79,6 +107,9 @@ namespace Chocobro {
       sw.Write(report);
       sw.Close();
     }
-
+    public double dpspercent(double abilitydamage, double dps, double fightlength) { 
+         double value = (((abilitydamage / fightlength) / dps) * 100);
+         return value;
+     }
   }
 }
